6654cb027d6abbff1cd6c4c6fb198b6959d57b0b
tainea <tainea@local>
2016-04-29 03:49:43 +0200

    add comments

--- js/myscript.js
+++ js/myscript.js
@@ -1,29 +1,38 @@
 var debug = true;
 
 $(document).ready(function(){
+	// DOM is ready, we can initialize our own stuff
 	init();
 });
 
 function
 init()
 {
-	$.stellar(); // PARALLAX
+	// enable PARALLAX effect on the header
+	$.stellar();
 
 	// if not in debug mode, we load data from an external server
 	if (debug) var dataUrl = 'json/data.json';
 	if (!debug) var dataUrl = 'http://node-test-nbwns.c9.io/discover_brussels/data/';
+
 	loadData(dataUrl);
 }
 
 function
 loadData(url)
 {
+	// start loading some data and call onDataLoaded when it is done
 	$.getJSON(url, onDataLoaded);
 }
 
 function
 onDataLoaded(data)
 {
+	// data is loaded, we can initialize our objects
+	
+	// the objects wrap themselves around some specific DOM elements
+	// which they will manage for us
+	// hence this format "new SomeObject($(el))"
 	var sidebar = new Sidebar($("#sidebar"));
 	new HighlightsList($("#highlights"), data.highlights, sidebar);
 	new HighlightsMap($("#map"), data.mapConfig, data.highlights, sidebar);
@@ -34,19 +43,22 @@ onDataLoaded(data)
 function
 HighlightsList(el, data, sidebar)
 {	
-
 	this._el = el;
 	this._data = data;
 	this._sidebare = sidebar;
 
+	// lodash provides a nice templating mechanism
 	var template = _.template('<div data-page-url="<%- pageUrl %>" class="thumbnail"><img src="<%- picture %>" alt="<%- name %>"><div class="caption"><h3><%- name %></h3><p><%- abstract %></p><p><a class="btn btn-primary" data="<%- name %>"">Learn more</a></p></div></div>');
 
+	// loop through the data, apply the template and display the results
 	_(data).forEach(function(o) {
 		el.append(template(o));
 	});
 
+	// bind click events to the "Learn More" buttons
 	el.find("a.btn").bind("click", {data: data, sidebar: sidebar}, function(event) {
 		var name = event.target.attributes.data.value;
+		// we use the name as a key to find the object to display
 		var o = _.find(event.data.data, {name: name} );
 		console.log(o);
 		event.data.sidebar.display(o);
@@ -95,6 +107,7 @@ HighlightsMap(el, config, data, sidebar)
 	this._data = data;
 	this._sidebar = sidebar;
 
+	// initialize google maps
 	this._map = new GMaps({
 		div: "#" + this._el.attr("id"),
 		lat: config.defaultLatitude,
@@ -102,6 +115,7 @@ HighlightsMap(el, config, data, sidebar)
 		zoom: config.defaultZoom
 	});
 
+	// place the markers
 	var m = this._map;
 	_(this._data).forEach(function(o) {
 		m.addMarker({
